5c9c258a60679fed23a798d436daefdd9f214877
MuelNova <muel@nova.gal>
2025-12-10 13:36:24 +0800

    feat: :sparkles: add avatar

--- src/consts.ts
+++ src/consts.ts
@@ -4,5 +4,6 @@
 export const SITE_TITLE = 'Muir\'s Console';
 export const SITE_DESCRIPTION = 'Hacking 4 fun!';
 export const USER_NAME = 'Muir Nova';
+export const AVATAR_URL = 'https://github.com/MuelNova.png';
 
 export const SITE_URL = 'https://astro.nova.gal';
