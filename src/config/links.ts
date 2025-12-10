545eac9c0eb4b40f099896d6cb18db0cbeabccac
MuelNova <muel@nova.gal>
2025-12-11 02:09:38 +0800

    feat: :sparkles: add friendLink

---
+++ src/config/links.ts
@@ -0,0 +1,56 @@
+export interface LinkCardProps {
+  link: string;
+  linkText: string;
+  description: string;
+  icon?: string;
+}
+
+const List: LinkCardProps[] = [
+  {
+    link: "https://silente.dev/",
+    linkText: "Shark Blog",
+    description:
+      "SilentE 是区块链大师，Web3 米恰飞起来了，又会 Web 安全，太厉害了",
+    icon: "https://silente.dev/favicon.svg",
+  },
+  {
+    link: "https://blog.hzao.top/",
+    linkText: "Hzao's Blog",
+    description:
+      "Hzao 是 Swift 大师，获得了 WWDC23 Swift Student Challenge 大奖，又去 Google 实习，太牛了",
+    icon: "https://secure.gravatar.com/avatar/d73560fa4904678be8395e90446b6ffb",
+  },
+  {
+    link: "https://blog.junyu33.me/",
+    linkText: "Junyu's Blog",
+    description:
+      "Junyu 是川大的密码学大师，曾经和我一起在 NUS 进行交流学习，PWN 水平也很高，实在是厉害。",
+    icon: "https://junyu33.me/img/avatar.jpg",
+  },
+  {
+    link: "https://rocketma.dev/",
+    linkText: "RocketMaDev",
+    description: "RocketMa，Pwner, Hack for Fun.",
+    icon: "https://rocketma.dev/images/avatar.png",
+  },
+  {
+    link: "https://www.haoqiguai.site/",
+    linkText: "奇怪的轩轩",
+    description: "同组的学弟，大二开始学 Pwn，进度非常快，喜欢奶龙（笑）",
+    icon: "https://img.notionusercontent.com/s3/prod-files-secure%2F647dfbeb-5527-4768-ae58-86e7a56d9e84%2Faa4b7b3e-c7ed-44f7-a747-0f5cbeb2d8bb%2F%E5%A5%B6%E9%BE%991.webp/size/w=800?exp=1765471005&sig=f1dKycUmusJmdbX6LpLw15NKSP12cmaVhm47VaQN1Zs&id=13cf358e-6f8b-8128-a832-000b901b75fa&table=collection&userId=c00961db-e27a-40f1-aa6e-8c40e80b89ee",
+  },
+  {
+    link: "https://www.fei3ei.xyz/",
+    linkText: "fei3ei",
+    description: "21 届 vidar-tream ( vidar-team? ) 的逆向手，目前在游戏安全",
+    icon: "https://www.fei3ei.xyz/favicon.ico"
+  },
+  {
+    link: "https://blog.kingbridge.one/",
+    linkText: "BRIdGE's blog",
+    description: "Gemini: 看起来不像那种由 AI 生成或者堆砌枯燥教程的「技术文档站」，这个博客更有「人味儿」。自称「赛博疯子」可能暗示了博主的涉猎范围很广，想法天马行空，或者对当下的科技生活有着某种狂热甚至戏谑的态度。",
+    icon: "https://blog.kingbridge.one//images/avatar.jpg"
+  }
+];
+
+export default List;
