db3cccc0fd9a08f99b587a0054cdfc747a3d0a49
MuelNova <muel@nova.gal>
2025-12-11 04:06:41 +0800

    chore: :wrench: update siteUrl

--- src/consts.ts
+++ src/consts.ts
@@ -6,4 +6,4 @@ export const SITE_DESCRIPTION = 'Hacking 4 fun!';
 export const USER_NAME = 'Muir Nova';
 export const AVATAR_URL = 'https://github.com/MuelNova.png';
 
-export const SITE_URL = 'https://astro.nova.gal';
+export const SITE_URL = 'https://nova.gal';
